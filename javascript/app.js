e45da183bd7489bef3775df54089a2bb4f0671f8
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-12-07 11:54:57 +1300

    adding files

--- javascript/app.js
+++ javascript/app.js
@@ -4,6 +4,15 @@
     function init() {
         var router = new Router([
             new Route('home', 'home.html', true),            
+            new Route('about', 'about.html'),
+            new Route('choose-character', 'choose-character.html'),
+            new Route('story-one', 'story-one.html'),
+            new Route('story-two', 'story-two.html'),
+            new Route('story-three', 'story-three.html'),
+            new Route('choices-one', 'choices-one.html'),
+            new Route('choices-one', 'choices-two.html'),
+            new Route('choices-one', 'choices-three.html'),
+            new Route('choice-result', 'choice-result.html'),
             new Route('about', 'about.html')
         ]);
     }
